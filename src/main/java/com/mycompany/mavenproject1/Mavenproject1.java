30f6b409740c80144c92d102da3e4e131ffdd47d
hamdyy04 <161123048+hamdyy04@users.noreply.github.com>
2025-03-03 15:05:33 +0200

    Add files via upload

---
+++ src/main/java/com/mycompany/mavenproject1/Mavenproject1.java
@@ -0,0 +1,9 @@
+package com.mycompany.mavenproject1;
+import java.awt.Dimension;
+public class Mavenproject1 {
+public static void main(String[] args) {
+jframe1 JF = new jframe1();
+JF.setVisible(true);
+JF.setMinimumSize(new Dimension(925,350));
+    }
+}

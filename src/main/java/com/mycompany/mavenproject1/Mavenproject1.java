1092280bb099582e2ad0cd1ce897d68239dceef7
hamdyy04 <161123048+hamdyy04@users.noreply.github.com>
2025-03-03 15:03:51 +0200

    Delete src directory

--- src/main/java/com/mycompany/mavenproject1/Mavenproject1.java
+++
@@ -1,9 +0,0 @@
-package com.mycompany.mavenproject1;
-import java.awt.Dimension;
-public class Mavenproject1 {
-public static void main(String[] args) {
-jframe1 JF = new jframe1();
-JF.setVisible(true);
-JF.setMinimumSize(new Dimension(925,350));
-    }
-}
